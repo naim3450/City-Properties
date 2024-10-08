e0602beafcb14ae0cb257e03d56b6b9fd203676b
sarkertarun <sarkertarun23@gmail.com>
2024-10-08 14:17:45 +0600

    City-Properties

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Navber from '../Component/Navber'
-import Hero from '../Component/Hero'
+
+// import AboutUs from '../Component/AboutUs'
+// import Navber from '../Component/Navber'
+// import Hero from '../Component/Hero'
+// import Footer from '../Component/Footer'
 
 const Home = () => {
     return (
         <div>
-            <Navber />
-            <Hero />
+            {/* <Navber />
+            <Hero /> */}
+            {/* <AboutUs/>
+            <Footer/> */}
         </div>
     )
 }
