a4413fdc7c7589fc22d3f6f719298c486dd3bcb2
naim3450 <naim.saim74@gmail.com>
2024-10-08 15:40:54 +0600

    City Properties

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navber from '../Component/Navber'
 import Hero from '../Component/Hero'
+import SlidePart from '../Component/SlidePart'
+import AboutUs from '../Component/AboutUs'
+import WorkPart from '../Component/WorkPart'
+import HappyClint from '../Component/HappyClint'
+import Footer from '../Component/Footer'
 
 const Home = () => {
     return (
         <div>
             <Navber />
             <Hero />
+            <SlidePart />
+            <AboutUs />
+            <WorkPart />
+            <HappyClint />
+            <Footer />
         </div>
     )
 }
