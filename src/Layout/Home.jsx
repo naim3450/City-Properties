ee0b3c5ad258581c564c3c88f0a03a9188be2427
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-10-09 11:30:09 +0600

    modified by rodro

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Navber from '../Component/Navber'
-import Hero from '../Component/Hero'
-import SlidePart from '../Component/SlidePart'
-import AboutUs from '../Component/AboutUs'
-import WorkPart from '../Component/WorkPart'
-import HappyClint from '../Component/HappyClint'
-import Footer from '../Component/Footer'
+import React from "react";
+import Navber from "../Component/Navber";
+import Hero from "../Component/Hero";
+import SlidePart from "../Component/SlidePart";
+import AboutUs from "../Component/AboutUs";
+import WorkPart from "../Component/WorkPart";
+import HappyClint from "../Component/HappyClint";
+import Footer from "../Component/Footer";
+import Properties from "../Component/Properties";
+import Subscribe from "../Component/Subscribe";
 
 const Home = () => {
-    return (
-        <div>
-            <Navber />
-            <Hero />
-            <SlidePart />
-            <AboutUs />
-            <WorkPart />
-            <HappyClint />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navber />
+      <Hero />
+      <SlidePart />
+      <AboutUs />
+      <WorkPart />
+      <Properties />
+      <Subscribe />
+      <HappyClint />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
