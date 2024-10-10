372fb1d1df06fcc16ce49931ee8a300675a4a75f
mdabutalebdev <mdabutaleb.dev@gmail.com>
2024-10-10 11:42:44 +0600

    ATK

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import WorkPart from '../Component/WorkPart'
 import HappyClint from '../Component/HappyClint'
-// import Navber from '../Component/Navber'
-// import Hero from '../Component/Hero'
+import Navber from '../Component/Navber'
+import Hero from '../Component/Hero'
 
 const Home = () => {
     return (
         <>
            
-            {/* <Navber /> */}
-            {/* <Hero /> */}
+            <Navber /> 
+            <Hero /> 
 
             <WorkPart />
             <HappyClint />
